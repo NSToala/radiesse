f1f798acbf538535db5411f14379b64959a4e4fc
Edgar Ramirez <edgartoala@gmail.com>
2022-02-03 23:00:23 -0600

    fix: navbar rapair

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -41,9 +41,9 @@ export const Navbar = () => {
 
             <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
                 <ul className="navbar-nav ml-auto">
-
+                    
                     <span className="nav-item nav-link text-info">
-                        BIENVENIDO, { user.user.username }
+                        BIENVENIDO, { user?.user?.username }
                     </span>
                     
                     <button 
